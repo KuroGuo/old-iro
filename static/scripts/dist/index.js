c271ed8ebd1fcf585fe0c1190b235aa700b5d5a6
KuroGuo <413298956@qq.com>
2015-02-08 00:55:24 +0800

    post link visited

--- static/scripts/dist/index.js
+++ static/scripts/dist/index.js
@@ -1,1 +1,1 @@
-define(["./app","vue"],function(t,e){"use strict";new e({el:"#post_list",data:{lastReadId:parseFloat(window.sessionStorage.lastReadId)},methods:{lastRead:function(t){window.sessionStorage.lastReadId=this.lastReadId=t}}}),new e({el:"#new_post_form_wrapper",data:{content:""},methods:{sync:function(t){this.content=t.target.innerHTML}}})});
+define(["./app","vue"],function(n,t){"use strict";new t({el:"#new_post_form_wrapper",data:{content:""},methods:{sync:function(n){this.content=n.target.innerHTML}}})});
--- static/scripts/src/index.js
+++ static/scripts/src/index.js
@@ -1,16 +1,4 @@
 define(['./app', 'vue'], function (app, Vue) { 'use strict';
-  var postListView = new Vue({
-    el: '#post_list',
-    data: {
-      lastReadId: parseFloat(window.sessionStorage.lastReadId)
-    },
-    methods: {
-      lastRead: function (id) {
-        window.sessionStorage.lastReadId = this.lastReadId = id;
-      }
-    }
-  });
-
   var newPostView = new Vue({
     el: '#new_post_form_wrapper',
     data: {
