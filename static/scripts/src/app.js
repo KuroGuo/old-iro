7c9856670d292002a43b85767cc237ba8494a09f
KuroGuo <413298956@qq.com>
2015-02-17 19:57:52 +0800

    temporary remove navigitor

--- static/scripts/src/app.js
+++ static/scripts/src/app.js
@@ -6,73 +6,73 @@ define(['vue', 'velocity'], function (Vue, Velocity) { 'use strict';
       document.documentElement.classList.add('touch');
   })();
 
-  ;(function () {
-    var navigitorView = new Vue({
-      el: '#navigitor',
-      data: {
-        navigitorShow: false,
-        scrollY: window.scrollY,
-        requestToken: null
-      },
-      ready: function () {
-        window.addEventListener('scroll', this.windowOnScroll);
-      },
-      methods: {
-        top: function () {
-          this.scrollTo(document.documentElement);
-        },
-        pre: function () {
-          var currentPost = this.getCurrentPost();
-          var prePost = currentPost.previousElementSibling;
-          this.scrollTo(prePost || currentPost);
-        },
-        next: function () {
-          var currentPost = this.getCurrentPost();
-          var nextPost = currentPost.nextElementSibling;
-          this.scrollTo(nextPost || currentPost);
-        },
-        preventDefault: function (e) {
-          e.preventDefault();
-        },
-        scrollTo: function (element) {
-          var view = this;
-          Velocity(element, 'scroll', {
-            duration: 1000,
-            mobileHA: false,
-            begin: function () {
-              window.removeEventListener('scroll', view.windowOnScroll);
-            },
-            complete: function () {
-              window.addEventListener('scroll', view.windowOnScroll);
-            }
-          });
-          view.navigitorShow = false;
-          view.scrollY = null;
-        },
-        getCurrentPost: function () {
-          var posts = document.querySelectorAll('#main .post');
-          var i;
-          console.log(posts);
-          for(i = 0; i < posts.length; i++) {
-            var top = posts[i].getBoundingClientRect(posts[i]).top;
-            if (top >= 0)
-              return posts[i - 1] || posts[i];
-          }
-          return posts[posts.length - 1];
-        },
-        windowOnScroll: function () {
-          var view = this;
-          if (!view.requestToken) {
-            view.requestToken = setTimeout(function () {
-              view.navigitorShow = window.scrollY <= view.scrollY;
+  // ;(function () {
+  //   var navigitorView = new Vue({
+  //     el: '#navigitor',
+  //     data: {
+  //       navigitorShow: false,
+  //       scrollY: window.scrollY,
+  //       requestToken: null
+  //     },
+  //     ready: function () {
+  //       window.addEventListener('scroll', this.windowOnScroll);
+  //     },
+  //     methods: {
+  //       top: function () {
+  //         this.scrollTo(document.documentElement);
+  //       },
+  //       pre: function () {
+  //         var currentPost = this.getCurrentPost();
+  //         var prePost = currentPost.previousElementSibling;
+  //         this.scrollTo(prePost || currentPost);
+  //       },
+  //       next: function () {
+  //         var currentPost = this.getCurrentPost();
+  //         var nextPost = currentPost.nextElementSibling;
+  //         this.scrollTo(nextPost || currentPost);
+  //       },
+  //       preventDefault: function (e) {
+  //         e.preventDefault();
+  //       },
+  //       scrollTo: function (element) {
+  //         var view = this;
+  //         Velocity(element, 'scroll', {
+  //           duration: 1000,
+  //           mobileHA: false,
+  //           begin: function () {
+  //             window.removeEventListener('scroll', view.windowOnScroll);
+  //           },
+  //           complete: function () {
+  //             window.addEventListener('scroll', view.windowOnScroll);
+  //           }
+  //         });
+  //         view.navigitorShow = false;
+  //         view.scrollY = null;
+  //       },
+  //       getCurrentPost: function () {
+  //         var posts = document.querySelectorAll('#main .post');
+  //         var i;
+  //         console.log(posts);
+  //         for(i = 0; i < posts.length; i++) {
+  //           var top = posts[i].getBoundingClientRect(posts[i]).top;
+  //           if (top >= 0)
+  //             return posts[i - 1] || posts[i];
+  //         }
+  //         return posts[posts.length - 1];
+  //       },
+  //       windowOnScroll: function () {
+  //         var view = this;
+  //         if (!view.requestToken) {
+  //           view.requestToken = setTimeout(function () {
+  //             view.navigitorShow = window.scrollY <= view.scrollY;
 
-              view.scrollY = window.scrollY;
+  //             view.scrollY = window.scrollY;
 
-              view.requestToken = null;
-            }, 100);
-          }
-        }
-      }
-    });
-  })();
+  //             view.requestToken = null;
+  //           }, 100);
+  //         }
+  //       }
+  //     }
+  //   });
+  // })();
 });
